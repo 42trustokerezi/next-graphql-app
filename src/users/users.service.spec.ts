c6e38979ab642459fd4d3e0657e00db5c32a9f5d
Trust Okerezi <trustokerezi42@gmail.com>
2021-11-22 23:57:26 +0100

    user service edited

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,25 +1,18 @@
-import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
-import { GetUserArgs } from './dto/args/get-user.args';
-import { CreateUserInput } from './dto/input/create-user.input';
-import { User } from './models/user';
+import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from './users.service';
 
-@Injectable()
-export class UsersService {
-    private users: User[] = [];
+describe('UsersService', () => {
+  let service: UsersService;
 
-    public createUser(createUserData: CreateUserInput): User {
-        const user: User = {
-            userId: uuidv4(),
-            ...createUserData
-        }
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService],
+    }).compile();
 
-        this.users.push(user);
+    service = module.get<UsersService>(UsersService);
+  });
 
-        return user;
-    }
-
-    public getUser(getUserArgs: GetUserArgs): User {
-        return this.users.find(user => user.userId === getUserArgs.userId);
-    }
-}
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
